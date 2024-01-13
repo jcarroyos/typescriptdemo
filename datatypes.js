c2f10781198202a4393a6b8e664bd937e41c7eb2
jcarroyos <jcarroyos@users.noreply.github.com>
2024-01-13 19:40:43 +0000

    codesanbox setup

--- datatypes.js
+++ datatypes.js
@@ -1,4 +1,24 @@
 "use strict";
-let lname = "Kumar";
+let lname = "Juan";
 let newname = lname.toUpperCase();
 console.log(newname);
+let age = "30";
+let result = parseInt(age);
+console.log(result);
+let isValid = false;
+console.log(isValid);
+let emptyList = [];
+emptyList = ["one", "two", "three"];
+let numList = [1, 2, 3, 4];
+//let results = numList.filter((num) => num > 2);
+let results = numList.find((num) => num > 2);
+console.log(results);
+let c = 2 /* Color.Blue */;
+console.log(c);
+let swapNumbs;
+function swapNumbers(num1, num2) {
+    return [num2, num1];
+}
+swapNumbs = swapNumbers(10, 20);
+swapNumbs[0];
+console.log(swapNumbs[0]);
--- datatypes.ts
+++ datatypes.ts
@@ -1,5 +1,29 @@
-let lname : string = "Kumar";
-
+let lname: string = "Juan";
 let newname = lname.toUpperCase();
-
 console.log(newname);
+let age = "30";
+let result = parseInt(age);
+console.log(result);
+let isValid: boolean = false;
+console.log(isValid);
+let emptyList: string[] = [];
+emptyList = ["one", "two", "three"];
+let numList: number[] = [1, 2, 3, 4];
+//let results = numList.filter((num) => num > 2);
+let results = numList.find((num) => num > 2);
+console.log(results);
+const enum Color {
+  Red,
+  Green,
+  Blue,
+}
+let c: Color = Color.Blue;
+console.log(c);
+
+let swapNumbs: [number, number];
+function swapNumbers(num1: number, num2: number): [number, number] {
+  return [num2, num1];
+}
+swapNumbs = swapNumbers(10, 20);
+swapNumbs[0];
+console.log(swapNumbs[0]);
